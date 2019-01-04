79edfdb416c05347f284d72e2afb1084aa944c4e
Alvin Antonius <alvin.antonius.siskom@gmail.com>
2019-01-04 12:15:05 +0800

    add gitignore and update spark-livy docker image

--- mesos/driver/entrypoint.sh
+++ mesos/driver/entrypoint.sh
@@ -5,21 +5,29 @@ SPARK_MASTER=${SPARK_MASTER:-"local"}
 LIVY_PORT=${LIVY_PORT:-"8998"}
 LIVY_SESSION_TIMEOUT=${LIVY_SESSION_TIMEOUT:-"2h"}
 SPARK_UI_PORT=${SPARK_UI_PORT:-'4040'}
-SPARK_DRIVER_PORT=${SPARK_DRIVER_PORT:-"23456"}
-SPARK_BLOCKMANAGER_PORT=${SPARK_BLOCKMANAGER_PORT:-"234567"}
-LIBPROCESS_PORT=${LIBPROCESS_PORT:-"23458"}
+SPARK_DRIVER_PORT=${SPARK_DRIVER_PORT:-"31001"}
+SPARK_BLOCKMANAGER_PORT=${SPARK_BLOCKMANAGER_ORT:-"31002"}
+LIBPROCESS_PORT=${LIBPROCESS_PORT:-"31003"}
 
-sed s/HOST_IP/$HOST_IP/ /usr/local/spark/conf/spark-env.sh > /usr/local/spark/conf/spark-env.sh
-sed s/LIBPROCESS_PORT/$LIBPROCESS_PORT/ /usr/local/spark/conf/spark-env.sh > /usr/local/spark/conf/spark-env.sh
+LIVY_ZOOKEEPER_STORE_URL=${LIVY_ZOOKEEPER_STORE_URL:-""}
+LIVY_RECOVERY_MODE="on"
+if [LIVY_ZOOKEEPER_STORE_URL == ""]; then
+	LIVY_RECOVERY_MODE="off"
+fi
+
+sed s/HOST_IP/$HOST_IP/ /usr/local/spark/conf/spark-env.sh >/usr/local/spark/conf/spark-env.sh
+sed s/LIBPROCESS_PORT/$LIBPROCESS_PORT/ /usr/local/spark/conf/spark-env.sh >/usr/local/spark/conf/spark-env.sh
 
 # SPARK_MASTER use `@` as delimiter since most likely it will contains `/`
-sed s@SPARK_MASTER@$SPARK_MASTER@ /usr/local/livy/conf/livy.conf > /usr/local/livy/conf/livy.conf
-sed s/LIVY_PORT/$LIVY_PORT/ /usr/local/livy/conf/livy.conf > /usr/local/livy/conf/livy.conf
-sed s/LIVY_SESSION_TIMEOUT/$LIVY_SESSION_TIMEOUT/ /usr/local/livy/conf/livy.conf > /usr/local/livy/conf/livy.conf
+sed s@SPARK_MASTER@$SPARK_MASTER@ /usr/local/livy/conf/livy.conf >/usr/local/livy/conf/livy.conf
+sed s/LIVY_PORT/$LIVY_PORT/ /usr/local/livy/conf/livy.conf >/usr/local/livy/conf/livy.conf
+sed s/LIVY_SESSION_TIMEOUT/$LIVY_SESSION_TIMEOUT/ /usr/local/livy/conf/livy.conf >/usr/local/livy/conf/livy.conf
+sed s/LIVY_ZOOKEEPER_STORE_URL/$LIVY_ZOOKEEPER_STORE_URL/ /usr/local/livy/conf/livy.conf >/usr/local/livy/conf/livy.conf
+sed s/LIVY_RECOVERY_MODE/$LIVY_RECOVERY_MODE/ /usr/local/livy/conf/livy.conf >/usr/local/livy/conf/livy.conf
 
-sed s@SPARK_MASTER@$SPARK_MASTER@ /usr/local/spark/conf/spark-defaults.conf > /usr/local/spark/conf/spark-defaults.conf
-sed s/SPARK_UI_PORT/$SPARK_UI_PORT/ /usr/local/spark/conf/spark-defaults.conf > /usr/local/spark/conf/spark-defaults.conf
-sed s/SPARK_DRIVER_PORT/$SPARK_DRIVER_PORT/ /usr/local/spark/conf/spark-defaults.conf > /usr/local/spark/conf/spark-defaults.conf
-sed s/SPARK_BLOCKMANAGER_PORT/$SPARK_BLOCKMANAGER_PORT/ /usr/local/spark/conf/spark-defaults.conf > /usr/local/spark/conf/spark-defaults.conf
+sed s@SPARK_MASTER@$SPARK_MASTER@ /usr/local/spark/conf/spark-defaults.conf >/usr/local/spark/conf/spark-defaults.conf
+sed s/SPARK_UI_PORT/$SPARK_UI_PORT/ /usr/local/spark/conf/spark-defaults.conf >/usr/local/spark/conf/spark-defaults.conf
+sed s/SPARK_DRIVER_PORT/$SPARK_DRIVER_PORT/ /usr/local/spark/conf/spark-defaults.conf >/usr/local/spark/conf/spark-defaults.conf
+sed s/SPARK_BLOCKMANAGER_PORT/$SPARK_BLOCKMANAGER_PORT/ /usr/local/spark/conf/spark-defaults.conf >/usr/local/spark/conf/spark-defaults.conf
 
 /usr/local/livy/bin/livy-server
--- mesos/driver/start_spark_driver.sh
+++ mesos/driver/start_spark_driver.sh
@@ -3,6 +3,5 @@ IP=$(hostname -i)
 SPARK_MASTER=mesos://zk://[zookeeper-hosts]/mesos
 docker rm -f spark_driver
 docker run -d --name=spark_driver -i \
-  -e HOST_IP=$IP \
-  -e SPARK_MASTER=$SPARK_MASTER spark_driver
-
+	-e HOST_IP=$IP \
+	-e SPARK_MASTER=$SPARK_MASTER spark_driver
